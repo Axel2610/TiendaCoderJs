3480e8054383a053f162fec87d2de90441fc66ed
Axel2610 <axelgarciamdp@hotmail.com>
2021-10-07 15:10:46 -0300

    AnimacionesJquery

--- js/contacto.js
+++ js/contacto.js
@@ -3,18 +3,24 @@ $(()=>{
     $("#inputEmail3").blur(function(){
         $("#inputEmail3").css("background-color", "#e1f7dc")
     })
+
+    $("#inputTelefono").blur(function(){
+        $("#inputTelefono").css("background-color", "#e1f7dc")
+    })
      
     $("#consulta").blur(function(){
         $(this).css("background-color", "#e1f7dc");
     })
 
+    $("#consulta").delay(1500).slideUp(2000).slideDown(2000);
+
     $("#divAgregado").append(
         `<form>
-        <div class="form-row">
-            <div class="col">
+        <div class="row">
+            <div class="col-6">
                  <input type="text" class="form-control" id="inputNombre" placeholder="Nombre">
             </div>
-            <div class="col">
+            <div class="col-6">
                  <input type="text" class="form-control" id="inputApellido" placeholder="Apellido">
             </div>
         </div>
